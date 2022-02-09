bc0602ec7431b26bd1d13ff69ee03b814457101d
BlackPrince163 <4523120100Ni>
2022-02-09 11:06:03 +0300

    + api weather

---
+++ src/main/java/com/khadimullin/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.khadimullin.controller;
+
+import com.khadimullin.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+public class HelloController {
+    private final Service service = new Service();
+
+    @GetMapping("/kazan")
+    public String hello(@RequestParam Optional<String> name) throws IOException {
+        return Service.get(name.orElse("Kazan"));
+    }
+}
