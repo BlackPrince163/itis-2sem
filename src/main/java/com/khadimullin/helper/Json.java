5f0b44a20fb9e833a793ff01381f86511f1651f4
BlackPrince163 <4523120100Ni>
2022-02-16 11:55:17 +0300

    + helper

---
+++ src/main/java/com/khadimullin/helper/Json.java
@@ -0,0 +1,23 @@
+package com.khadimullin.helper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Json {
+    public Map<String, String> parseJson(String json) throws JsonProcessingException {
+        Map<String, String> map = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+
+        map.put("description", jsonNode.get("weather").get(0).get("description").asText());
+        map.put("temp", jsonNode.get("main").get("temp").asText());
+        map.put("name", jsonNode.get("name").asText());
+
+        return map;
+    }
+}
