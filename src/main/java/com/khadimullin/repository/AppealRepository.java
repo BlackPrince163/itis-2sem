4324178e838383aebcd131452409f60c2c67e9af
BlackPrince163 <4523120100Ni>
2022-03-02 11:45:11 +0300

    + task 1

---
+++ src/main/java/com/khadimullin/repository/AppealRepository.java
@@ -0,0 +1,7 @@
+package com.khadimullin.repository;
+
+import com.khadimullin.model.Appeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppealRepository extends JpaRepository<Appeal, Integer> {
+}
