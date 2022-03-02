b7f86a91c3efe8df700aca3f0228827fa607bf15
BlackPrince163 <4523120100Ni>
2022-03-02 12:06:06 +0300

    + controller

--- src/main/java/com/khadimullin/controller/WeatherController.java
+++ src/main/java/com/khadimullin/controller/WeatherController.java
@@ -1,25 +1,107 @@
-//package com.khadimullin.controller;
-//
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.khadimullin.dto.CreateHistoryDto;
-//import com.khadimullin.dto.HistoryDto;
-//import com.khadimullin.helper.HistoryHelper;
-//import com.khadimullin.helper.Json;
-//import com.khadimullin.model.HistoryWeather;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//import javax.validation.Valid;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Optional;
-//
-//public class WeatherController {
-//}
-//
-//    @GetMapping("/history")
-//    public List<HistoryDto> getWeather() {
-//        return HistoryDto.fromModel();
-//    }
-//}
+package com.khadimullin.controller;
+
+import com.khadimullin.model.Appeal;
+import com.khadimullin.model.HistoryWeather;
+import com.khadimullin.model.User;
+import com.khadimullin.repository.AppealRepository;
+import com.khadimullin.repository.HistoryRepository;
+import com.khadimullin.repository.UserRepository;
+import com.khadimullin.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class WeatherController {
+    private final Service weatherService = new Service();
+    private final HistoryRepository historyRepository;
+    private final UserRepository userRepository;
+    private final AppealRepository appealRepository;
+
+
+    @Autowired
+    public WeatherController(HistoryRepository historyRepository, UserRepository userRepository, AppealRepository appealRepository) {
+        this.historyRepository = historyRepository;
+        this.userRepository = userRepository;
+        this.appealRepository = appealRepository;
+    }
+
+    @GetMapping("/weather")
+    public HistoryWeather addWeather(@Valid @RequestParam Optional<String> city, @RequestParam int user_id) throws IOException {
+        Map<String, String> result = weatherService.get(city.orElse("Kazan"));
+        HistoryWeather weather = new HistoryWeather(result.get("name"), result.get("temp"), result.get("feels_like"), result.get("wind_speed"), new Date().getTime());
+        appealRepository.save(new Appeal(userRepository.getById(user_id), weather, result.get("name")));
+        return historyRepository.save(weather);
+    }
+
+    @GetMapping("/getAllRequestById")
+    public String getAllRequestByID(@RequestParam int id) {
+        List<Appeal> requestList = appealRepository.findAll();
+        String result = "";
+        for (Appeal request : requestList) {
+            if (request.getUser().getId() == id) {
+                result += request.toString() + "\n";
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("/getAllRequestByCity")
+    public String getAllRequestByCity(@RequestParam String city) {
+        List<Appeal> requestList = appealRepository.findAll();
+        String result = "";
+        for (Appeal appeal : requestList) {
+            if (appeal.getCity().equals(city)) {
+                result += appeal.toString() + "\n";
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("/getAllWeatherByCity")
+    public String getAllWeatherByCity(@RequestParam String city) {
+        List<HistoryWeather> weatherList = historyRepository.findAll();
+        String result = "";
+        for (HistoryWeather weather : weatherList) {
+            if (weather.getCity().equals(city)) {
+                result += weather.toString() + "\n";
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("/getWeather")
+    public String getWeather(@RequestParam String email) {
+        List<User> users = userRepository.findAll();
+        List<HistoryWeather> weatherList = historyRepository.findAll();
+        String result = "";
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                for (HistoryWeather weather : weatherList) {
+                    result += weather.toString() + "\n";
+                }
+            }
+        }
+        return result;
+    }
+
+    @GetMapping("/getAll")
+    public String getAll(){
+        List<User> users = userRepository.findAll();
+        List<HistoryWeather> weatherList = historyRepository.findAll();
+        String result = "";
+        for (User user : users) {
+            result += user.toString() + "\n";
+        }
+        for (HistoryWeather weather : weatherList) {
+            result += weather.toString()+"\n";
+        }
+        return result;
+    }
+}
