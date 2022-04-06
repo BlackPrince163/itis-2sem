ca085c54d2fab8a43ce17ec8e3c2497aa8558f65
BlackPrince163 <4523120100Ni>
2022-04-07 00:25:25 +0300

    + done x2

---
+++ src/main/java/com/khadimullin/aspect/CountAspect.java
@@ -0,0 +1,46 @@
+package com.khadimullin.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+@Slf4j
+public class CountAspect {
+
+    private final static Map<String,Integer> amountOfCalls = new HashMap<>();
+
+    @Pointcut("execution(* ru.itis.ruzavin.*.*.*(..))")
+    public void logAmountMethodsExecution() {
+
+    }
+
+    @Around("logAmountMethodsExecution()")
+    public Object logAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String methodName = methodSignature.getName();
+
+        Integer amountBefore = amountOfCalls.get(methodName);
+
+        if (amountBefore == null) {
+            amountOfCalls.put(methodName, 1);
+        } else {
+            amountOfCalls.put(methodName, amountBefore + 1);
+        }
+
+        Integer amountAfter = amountOfCalls.get(methodName);
+
+        log.info("method with signature {} calls {} times",methodName, amountAfter);
+
+        return proceedingJoinPoint.proceed();
+    }
+}
